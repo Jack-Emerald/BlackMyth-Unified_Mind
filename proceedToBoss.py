e235ad67263777ca99b2a9d56761ff351febd2e2
Zixuan Chen <zixuac7@uci.edu>
2025-03-15 00:57:00 -0700

    redeploy the video

--- proceedToBoss.py
+++ proceedToBoss.py
@@ -22,7 +22,7 @@ class proceedToBoss:
             print("Fight Yellow Loong with challenge mode")
             self.boss()
         elif self.BOSS == 3: # Challenge mode bosses
-            print("Fight Yin Tiger with challenge mode")
+            print("Fight Chiranlong with challenge mode")
             self.boss()
         else:
             print("ProceedToBoss Configuration Not Found")
